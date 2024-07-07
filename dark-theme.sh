444bf9a6c27b8de2e2bb0b4ef31306e38ed47156
rute <90400844+nbycoder@users.noreply.github.com>
2024-07-07 16:16:21 +0100

    Update and rename suntimes.py to dark-theme.sh

---
+++ dark-theme.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+# VARIABLES
+
+# THEME
+theme_DIR="$HOME/.config/cosmic/com.system76.CosmicTheme.Mode/v1/is_dark"
+theme_light="false" 
+theme_dark="true"
+
+# WALLPAPER
+bg_DIR="$HOME/.config/cosmic/com.system76.CosmicBackground/v1/all"
+bg_light="su-light.png" # CHANGE TO YOUR WALLAPAPER NAME 
+bg_dark="su-dark.jpeg" # CHANGE TO YOUR WALLAPAPER NAME
+
+# ICONS
+icons_DIR="$HOME/.config/cosmic/com.system76.CosmicTk/v1/icon_theme"
+icons_light='"rose-pine-dawn"' # CHANGE TO YOUR ICON THEME NAME
+icons_dark='"rose-pine-moon"' # CHANGE TO YOUR ICON THEME NAME
+
+# CODE
+
+sed -i -e "s/$theme_light/$theme_dark/g" "$theme_DIR" # CHANGE THEME
+sed -i -e "s/$bg_light/$bg_dark/g" "$bg_DIR" # CHANGE WALLPAPER
+sed -i -e "s/$icons_light/$icons_dark/g" "$icons_DIR" # CHANGE ICONS
